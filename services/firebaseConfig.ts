d42af42399e4eed6c3d58ff2d1303ea4ab285ddf
ivan babida <babidaivan09@gmail.com>
2025-10-06 19:42:38 +0800

    fix minor issues

--- services/firebaseConfig.ts
+++ services/firebaseConfig.ts
@@ -13,7 +13,7 @@ export const firebaseConfig = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: "rescue-app-6f6e4.firebaseapp.com",
     projectId: "rescue-app-6f6e4",
-    storageBucket: "rescue-app-6f6e4..appspot.com", //firebasestorage.app
+    storageBucket: "rescue-app-6f6e4.appspot.com", //firebasestorage.app
     messagingSenderId: "992924613336",
     appId: "1:992924613336:web:a20041d90680e7d1f575ad"
 };
